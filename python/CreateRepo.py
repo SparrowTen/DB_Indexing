f484b78379fd911c068ddf72f468b6118bed51e2
Poyu <42506064+poyu39@users.noreply.github.com>
2023-05-29 15:51:20 +0800

    創建 CreateRepo
    分割單位 kb

---
+++ python/CreateRepo.py
@@ -0,0 +1,68 @@
+'''
+實作資料 repository:
+
+將原始資料檔案以 block 為單位切割分別儲存
+單一 block 最多只能存放一百筆選課資料或上限 8KB 的索引資料
+DEMO 時不需要展示分割 repository 的程式
+僅需說明 repository 產生的方式即可
+'''
+
+import os
+import csv
+
+class Repository:
+    def __init__(self, filename):
+        self.filename = filename
+        self.block_count = 0
+        self.workspace = os.path.dirname(__file__).split('python')[0]
+        if not os.path.exists(self.workspace + '/data/' + filename):
+            print('錯誤! 沒有該檔案')
+            os._exit(0)
+        
+        # 創建 repository 資料夾
+        if not os.path.exists(self.workspace + '/repository'):
+            os.mkdir(self.workspace + '/repository')
+
+    def getlength(self):
+        with open(self.workspace + '/data/' + self.filename, 'r', encoding='utf-8') as csvfile:
+            raw_data = csv.reader(csvfile)
+            self.length = len(list(raw_data))
+        return self.length
+    
+    def getBlockCount(self):
+        return self.block_count
+    
+    def split(self, size_limit):
+        size_limit = size_limit * 1024
+        block_index = 0
+        buffer = 0
+        data = []
+        with open(self.workspace + '/data/' + self.filename, 'r', encoding='utf-8') as csvfile:
+            rows = csv.reader(csvfile)
+            for row in rows:
+                buffer += len(str(row).encode('utf-8'))
+                if buffer > size_limit:
+                    self.create_file('block' + str(block_index) + '.csv', data)
+                    block_index += 1
+                    buffer = 0
+                    data = []
+                data.append(row)
+            
+    def create_file(self, filename, data):
+        with open(self.workspace + '/repository/' + filename, 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in data:
+                writer.writerow(row)
+        print('已建立 ' + filename)
+        self.block_count += 1
+    
+if __name__ == '__main__':
+    print('請輸入檔案名稱')
+    filename = input()
+    r = Repository(filename)
+    print('資料總共' + str(r.getlength()) + '筆')
+    print('請輸入每個 block 的上限大小 (KB)')
+    size_limit = int(input())
+    r.split(size_limit)
+    print('共分割成' + str(r.getBlockCount()) + '個 block ')
+    print('每個 block 最多' + str(size_limit) + 'KB')
