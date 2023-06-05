737784f85110f364e78c77f48da5d844bc4b5700
Poyu <42506064+poyu39@users.noreply.github.com>
2023-06-05 21:27:30 +0800

    簡化 seq_index 索引表

--- python/CreateIndex.py
+++ python/CreateIndex.py
@@ -12,12 +12,12 @@ import numpy as np
 class Block:
     def __init__(self, filename):
         self.filename = filename
-        self.blockname = filename.split('.')[0]
+        self.blocknum = filename.split('.')[0].split('block')[1]
         self.workspace = os.path.dirname(__file__).split('python')[0]
     
     def getStudentID(self):
         student_id_list = []
-        with open(self.workspace + '/repository/' + self.filename, 'r', encoding='utf-8') as csvfile:
+        with open(self.workspace + '/raw_repo/' + self.filename, 'r', encoding='utf-8') as csvfile:
             rows = csv.reader(csvfile)
             for row in rows:
                 if row[0][0] == 'D':
@@ -33,25 +33,60 @@ class Block:
         args = np.argsort(data)
         output = []
         for i in range(len(array)):
-            output.append([array[i], self.blockname + '_' + str(args[i])])
+            output.append([array[i], self.blocknum + '_' + str(args[i])])
         return output
     
 if __name__ == '__main__':
-    workspace = os.path.dirname(__file__).split('python')[0] + '/repository/'
-    files = os.listdir(workspace)
-    # print(files)
-    # print('Total: ' + str(len(files)) + ' files')
-    # os._exit(0)
-    mergedBlock = np.array([['', '']])
-    for i in range(len(files)):
-        file = files[i]
-        block = Block(file)
-        raw_array = block.getStudentID()
-        seq_array = block.getSeqIndex(raw_array)
-        mergedBlock = np.concatenate((mergedBlock, seq_array))
-        print('Block ' + str(i) + ' finished')
-    mergedBlock = np.sort(mergedBlock, axis=0)
-    print(mergedBlock)
-    with open (block.workspace + '/index/DB_student_cls_SeqIndex.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    workspace = os.path.dirname(__file__).split('python')[0]
+    # 建立原始 seq_index 檔案
+    if not os.path.exists(os.path.dirname(__file__).split('python')[0] + '/index/DB_student_cls_SeqIndex.csv'):
+        raw_repo_dir = workspace + '/raw_repo/'
+        files = os.listdir(raw_repo_dir)
+        ## 合併 Block 檔案
+        mergedBlock = np.array([['', '']])
+        for i in range(len(files)):
+            file = files[i]
+            block = Block(file)
+            raw_array = block.getStudentID()
+            seq_array = block.getSeqIndex(raw_array)
+            mergedBlock = np.concatenate((mergedBlock, seq_array))
+            print('Block ' + str(i) + ' finished')
+        
+        ## 寫入原始 seq_index 檔案
+        with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerows(mergedBlock)
+    
+    # 精簡化 seq_index 檔案
+    ## 讀取原始 seq_index 檔案
+    seq_index = np.array([])
+    new_seq_index = np.array([['', '']])
+    with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'r', encoding='utf-8') as csvfile:
+        seq_index = np.array(list(csv.reader(csvfile)))
+    
+    ## 取得不重複的學號
+    student_id_arr = set(seq_index[:, 0])
+    student_id_arr = np.array(list(student_id_arr))
+    student_id_arr = np.sort(student_id_arr)
+    # print(student_id_arr)
+    
+    ## 以學號為 key，將所有課程編號合併成一個字串
+    for i in range(len(student_id_arr)):
+        student_id = student_id_arr[i]
+        student_all_class = ''
+        for i in range(len(seq_index)):
+            if seq_index[i][0] == student_id:
+                student_all_class += seq_index[i][1] + '-'
+        student_all_class = student_all_class.rstrip('-')
+        new_seq_index = np.concatenate((new_seq_index, [[student_id, str(student_all_class)]]))
+        if len(student_all_class) != 0:
+            print(student_id + ',' + student_all_class)
+        else:
+            print('no class')
+    
+    print(new_seq_index)
+    
+    ## 寫入精簡化 seq_index 檔案
+    with open (workspace + '/index/DB_student_cls_SeqIndex_new.csv', 'w', encoding='utf-8', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerows(mergedBlock)
+        writer.writerows(new_seq_index)
