7b0019044965be0fb75cc354d45089607e8c7c23
Poyu <42506064+poyu39@users.noreply.github.com>
2023-06-06 02:04:41 +0800

    優化 seq_index 創建 (200~it/s)

--- python/CreateIndex.py
+++ python/CreateIndex.py
@@ -8,24 +8,22 @@
 import os
 import csv
 import numpy as np
+from tqdm import tqdm, trange
 
 class Block:
-    def __init__(self, filename):
+    def __init__(self, dir, filename):
+        self.dir = '\\' + dir + '\\'
         self.filename = filename
         self.blocknum = filename.split('.')[0].split('block')[1]
         self.workspace = os.path.dirname(__file__).split('python')[0]
     
     def getStudentID(self):
         student_id_list = []
-        with open(self.workspace + '/raw_repo/' + self.filename, 'r', encoding='utf-8') as csvfile:
+        with open(self.workspace + self.dir + self.filename, 'r', encoding='utf-8') as csvfile:
             rows = csv.reader(csvfile)
             for row in rows:
                 if row[0][0] == 'D':
                     student_id_list.append(row[0])
-        # set 去除重複
-        # return set(student_id_list)
-        
-        # numpy sort
         return np.array(student_id_list)
     
     def getSeqIndex(self, data):
@@ -35,58 +33,65 @@ class Block:
         for i in range(len(array)):
             output.append([array[i], self.blocknum + '_' + str(args[i])])
         return output
-    
+
 if __name__ == '__main__':
+    # 程式資料夾工作區
     workspace = os.path.dirname(__file__).split('python')[0]
+    dir = '\\raw_block\\'
+    
     # 建立原始 seq_index 檔案
-    if not os.path.exists(os.path.dirname(__file__).split('python')[0] + '/index/DB_student_cls_SeqIndex.csv'):
-        raw_repo_dir = workspace + '/raw_repo/'
+    
+    if not os.path.exists(os.path.dirname(__file__).split('python')[0] + '/index/seq_student_cls.csv'):
+        raw_repo_dir = workspace + dir
         files = os.listdir(raw_repo_dir)
-        ## 合併 Block 檔案
+        create_block_process = tqdm(total=len(files), desc='分割 block ', ncols=100)
+        # 合併 Block 檔案
         mergedBlock = np.array([['', '']])
         for i in range(len(files)):
             file = files[i]
-            block = Block(file)
+            block = Block(dir, file)
             raw_array = block.getStudentID()
             seq_array = block.getSeqIndex(raw_array)
             mergedBlock = np.concatenate((mergedBlock, seq_array))
-            print('Block ' + str(i) + ' finished')
+            create_block_process.update(1)
+            # print('Block ' + str(i) + ' finished')
         
-        ## 寫入原始 seq_index 檔案
-        with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'w', encoding='utf-8', newline='') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerows(mergedBlock)
+        # 寫入原始 seq_index 檔案
+        # with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'w', encoding='utf-8', newline='') as csvfile:
+        #     writer = csv.writer(csvfile)
+        #     writer.writerows(mergedBlock)
     
-    # 精簡化 seq_index 檔案
-    ## 讀取原始 seq_index 檔案
-    seq_index = np.array([])
-    new_seq_index = np.array([['', '']])
-    with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'r', encoding='utf-8') as csvfile:
-        seq_index = np.array(list(csv.reader(csvfile)))
+    # 讀取原始 seq_index 檔案
+    seq_index = mergedBlock
+    # with open (workspace + '/index/DB_student_cls_SeqIndex.csv', 'r', encoding='utf-8') as csvfile:
+    #     seq_index = np.array(list(csv.reader(csvfile)))
     
-    ## 取得不重複的學號
+    # 取得不重複的學號
     student_id_arr = set(seq_index[:, 0])
     student_id_arr = np.array(list(student_id_arr))
     student_id_arr = np.sort(student_id_arr)
-    # print(student_id_arr)
     
-    ## 以學號為 key，將所有課程編號合併成一個字串
-    for i in range(len(student_id_arr)):
-        student_id = student_id_arr[i]
-        student_all_class = ''
-        for i in range(len(seq_index)):
-            if seq_index[i][0] == student_id:
-                student_all_class += seq_index[i][1] + '-'
-        student_all_class = student_all_class.rstrip('-')
-        new_seq_index = np.concatenate((new_seq_index, [[student_id, str(student_all_class)]]))
-        if len(student_all_class) != 0:
-            print(student_id + ',' + student_all_class)
-        else:
-            print('no class')
+    # 創建進度條
+    simple_process = tqdm(total=len(student_id_arr), desc='簡化 seq_index 檔案', ncols=100)
     
-    print(new_seq_index)
-    
-    ## 寫入精簡化 seq_index 檔案
-    with open (workspace + '/index/DB_student_cls_SeqIndex_new.csv', 'w', encoding='utf-8', newline='') as csvfile:
+    # 寫入精簡化 seq_index 檔案
+    with open (workspace + '/index/seq_student_cls.csv', 'w', encoding='utf-8', newline='') as csvfile:
         writer = csv.writer(csvfile)
-        writer.writerows(new_seq_index)
+        
+        # 遍歷所有學號
+        for i in range(len(student_id_arr)):
+            student_id = student_id_arr[i]
+            student_all_class_list = []
+            all_class_index = np.where(seq_index[:,:] == student_id)[0].tolist()
+            
+            for j in range(len(all_class_index)):
+                student_all_class_list.append(seq_index[all_class_index[j]][1])
+            student_all_class_list = np.sort(student_all_class_list)
+            
+            temp = []
+            for j in range(len(student_all_class_list)):
+                temp.append([student_id, student_all_class_list[j]])
+            writer.writerows(temp)
+            
+            # 更新進度條
+            simple_process.update(1)
