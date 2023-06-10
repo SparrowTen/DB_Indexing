6663b578b4bcc53a7f01a50ffad6b20a15c512d7
Poyu <42506064+poyu39@users.noreply.github.com>
2023-06-10 19:29:26 +0800

    新增 bucket index 碰撞平行探測

--- python/CreateHashIndex.py
+++ python/CreateHashIndex.py
@@ -8,10 +8,12 @@ class HashIndex:
         self.workspace = os.path.dirname(__file__).split('python')[0]
         self.source = self.workspace + source
         self.dir = '\\repository\\hash_index_block\\'
+        if not os.path.exists(self.workspace + self.dir):
+            os.makedirs(self.workspace + self.dir)
     
     def getKeySet(self, key_col=0):
         raw_blocks = os.listdir(self.source)
-        process = tqdm(total=len(raw_blocks), desc='從 raw_block 取得 keyset', ncols=100)
+        process = tqdm(total=len(raw_blocks), desc='raw_block -> key_set', ncols=100)
         key_set = set()
         for raw_block in raw_blocks:
             with open(self.source + raw_block, 'r', encoding='utf-8') as csvfile:
@@ -21,10 +23,10 @@ class HashIndex:
             process.update(1)
         return key_set
     
-    def getChunk(self, key_set, key_col=0):
+    def addValue(self, key_set, key_col=0):
         raw_blocks = os.listdir(self.source)
-        process = tqdm(total=len(key_set) + len(raw_blocks), desc='從 raw_block 取得 chunks', ncols=100)
-        chunks = {}
+        process = tqdm(total=len(raw_blocks), desc='key -> value', ncols=100)
+        bucket = {}
         for raw_block in raw_blocks:
             with open(self.source + raw_block, 'r', encoding='utf-8') as csvfile:
                 raw_repo = list(csv.reader(csvfile))
@@ -32,13 +34,83 @@ class HashIndex:
                 for key in key_set:
                     for row in raw_repo:
                         if key == row[key_col]:
-                            own_value.append(row)
-                    chunks[key] = own_value
+                            own_value.append(row[0])
+                    bucket[key] = own_value
             process.update(1)
-        return chunks
+        return bucket
     
+    def hashfunc(self, key, key_set):
+        return int(key) % len(key_set)
+    
+    def addHashIndex(self, key_value):
+        blocks = {}
+        key_set = list(key_value.keys())
+        for key in key_set:
+            hash_index = self.hashfunc(key, key_set)
+            bucket = {}
+            bucket['key'] = key
+            bucket['value'] = key_value[key]
+            bucket['pointer'] = None
+            if blocks.get(hash_index) != None:
+                linked_index = self.fixConflict(hash_index, blocks)
+                blocks[hash_index]['pointer'] = linked_index
+                blocks[linked_index] = bucket
+            elif blocks.get(hash_index) == None:
+                blocks[hash_index] = bucket
+        return blocks
+    
+    def fixConflict(self, index, blocks:dict):
+        # 平行探測 [+-1, +-2, +-3, ...]
+        new_index = 1
+        while True:
+            if blocks.get(index + new_index) == None:
+                index = index + new_index
+                break
+            if blocks.get(index - new_index) == None:
+                index = index - new_index
+                break
+            index += 1
+        return index
+
+def search(key, data):
+    index = hash_index.hashfunc(key, key_set)
+    return data[index]
+
+def search(key, key_set, data):
+    index = hash_index.hashfunc(key, key_set)
+    if data[index]['key'] == key:
+        return data[index]
+    else:
+        while True:
+            if data[index]['key'] != key:
+                if data[index]['pointer'] == None:
+                    return None
+                index = data[index]['pointer']
+            else:
+                return data[index]
+
 if __name__ == '__main__':
     hash_index = HashIndex()
-    key_set = hash_index.getKeySet(key_col=1)
-    chunks = hash_index.getChunk(key_set, key_col=1)
-    pprint.pprint(chunks['2142'])
+    if not os.path.exists(hash_index.workspace + '\\index\\' + 'hash_key.csv'):
+        key_set = hash_index.getKeySet(key_col=1)
+        with open(hash_index.workspace + '\\index\\' + 'hash_key.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for key in key_set:
+                writer.writerow([key])
+    if not os.path.exists(hash_index.workspace + '\\index\\' + 'hash_index.csv'):
+        key_value = hash_index.addValue(key_set, key_col=1)
+        blocks = hash_index.addHashIndex(key_value)
+        with open(hash_index.workspace + '\\index\\' + 'hash_index.csv', 'w', encoding='utf-8', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            for index in blocks:
+                writer.writerow([index, blocks[index]['key'], blocks[index]['value'], blocks[index]['pointer']])
+    
+    key_set = []
+    with open(hash_index.workspace + '\\index\\' + 'hash_key.csv', 'r', encoding='utf-8') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            key_set.append(row)
+    
+    with open(hash_index.workspace + '\\index\\' + 'hash_index.csv', 'r', encoding='utf-8') as csvfile:
+        data = csv.reader(csvfile)
+        print(search('2148', key_set, data))
